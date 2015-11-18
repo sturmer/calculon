c3e39164c307efa748dbd2e2c300ff22cec1c47b
sturmer <galiziacentrale@gergel.im>
2015-11-18 07:19:31 +0100

    Make buttons larger

--- calculon.rkt
+++ calculon.rkt
@@ -2,6 +2,12 @@
 
 ;; A simple calculator
 
+
+;; TODO
+;; - Avoid these horrible set!'s
+;; - Define a button with min-width 30 and use it for the buttons
+
+
 (define current-number 0)
 (define op1 0)
 (define op2 0)
@@ -16,7 +22,9 @@
     [(equal? fname "*") *]))
 
 (define (push-number button)
-  (set! current-number (+ (* current-number 10) (string->number (send button get-label))))
+  (set! current-number (+ 
+                        (* current-number 10) 
+                        (string->number (send button get-label))))
   (send display$ set-value (number->string current-number)))
 
 (define (push-operation button)
@@ -34,23 +42,29 @@
 ;; Row 1: 9 8 7 +
 (define row1 (new horizontal-panel% [parent frame]))
 
+;; ASK: I want a button whose min-width is always 30. I'm afraid 
+;; I don't know how to use inheritance (yet).
 (new button% [parent row1]
      [label "9"]
+     [min-width 30]
      [callback (lambda (button event)
                  (push-number button))])
 
 (new button% [parent row1]
      [label "8"]
+     [min-width 30]
      [callback (lambda (button event)
                  (push-number button))])
 
 (new button% [parent row1]
      [label "7"]
+     [min-width 30]
      [callback (lambda (button event)
                  (push-number button))])
 
 (new button% [parent row1]
      [label "+"]
+     [min-width 30]     
      [callback (lambda (button event)
                  (push-operation button))])
 
@@ -59,21 +73,25 @@
 
 (new button% [parent row2]
      [label "6"]
+     [min-width 30]     
      [callback (lambda (button event)
                  (push-number button))])
 
 (new button% [parent row2]
      [label "5"]
+     [min-width 30]
      [callback (lambda (button event)
                  (push-number button))])
 
 (new button% [parent row2]
      [label "4"]
+     [min-width 30]
      [callback (lambda (button event)
                  (push-number button))])
 
 (new button% [parent row2]
      [label "-"]
+     [min-width 30]
      [callback (lambda (button event)
                  (push-operation button))])
 
@@ -82,21 +100,25 @@
 
 (new button% [parent row3]
      [label "3"]
+     [min-width 30]
      [callback (lambda (button event)
                  (push-number button))])
 
 (new button% [parent row3]
      [label "2"]
+     [min-width 30]
      [callback (lambda (button event)
                  (push-number button))])
 
 (new button% [parent row3]
      [label "1"]
+     [min-width 30]
      [callback (lambda (button event)
                  (push-number button))])
 
 (new button% [parent row3]
      [label "*"]
+     [min-width 30]
      [callback (lambda (button event)
                  (push-operation button))])
 
@@ -105,16 +127,19 @@
 
 (new button% [parent row4]
      [label "0"]
+     [min-width 30]
      [callback (lambda (button event)
                  (push-number button))])
 
 (new button% [parent row4]
      [label "."]
+     [min-width 30]
      [callback (lambda (button event)
                  (set! decimal-point #t))])
 
 (new button% [parent row4]
      [label "="]
+     [min-width 30]
      [callback (lambda (button event)
                  (set! op2 current-number)
                  (set! current-number ((get-function current-operation) op1 op2))
@@ -124,6 +149,7 @@
 
 (new button% [parent row4]
      [label "/"]
+     [min-width 30]
      [callback (lambda (button event)
                  (push-operation button))])
 
