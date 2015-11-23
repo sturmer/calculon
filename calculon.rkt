a61b705bc76030d2094614588dd73da24001c161
sturmer <galiziacentrale@gergel.im>
2015-11-23 21:27:35 +0100

    Match and parse simple expressions
    Still need to address fractional maths to make it working properly.

--- calculon.rkt
+++ calculon.rkt
@@ -2,20 +2,18 @@
 
 ;; A simple calculator
 
-
 ;; TODO
 ;; - Keep the current-number a string to which we append until we need to use 
 ;;     it as a number
 ;; - Define a button with min-width 30 and use it for the buttons
-;; - Use x and the real division sign for the respective operations
 ;; - Allow expressions with parenthesis
 
 (define (get-function fname)
   (match fname
     ["+" +]
     ["-" -]
-    ["*" *]
-    ["/" /]))
+    ["×" *]
+    ["÷" /]))
 
 (define (push-number button)
   (define button-value (send button get-label))
@@ -26,25 +24,37 @@
             (match current-display    
               [(regexp #rx"^0") button-value]
               [_ (string-append current-display button-value)])])
+       ;(printf "Pushed ~a\n" button-value)
        (send display$ set-value result))]
     [_ (send display$ set-value current-display)]))
 
 (define (push-operation button)  
   (define acc (send display$ get-value))
   (define current-operation (send button get-label))
+  ;(printf "Pushed ~a\n" current-operation)
   (send display$ set-value (string-append acc current-operation)))
 
-(define (push-equal)
+(define (push-equal)  
+  (define expr (send display$ get-value))
+  (printf "expr = '~a'\n" expr)
+  (define ops-lst (regexp-match
+                   #px"(\\d+)\\s*([-÷+×])\\s*(\\d+)"
+                   expr))
   (define result 0)
-  (send display$ set-value (number->string result)))
+  (match ops-lst
+    [(list _ x f y)
+     (send display$ set-value (number->string
+                               ((get-function f)
+                                (string->number x)
+                                (string->number y))))]
+    [_ (raise exn:fail:user)]))
 
 (define frame (new frame% [label "Calculon"]))
 
 (define display$ (new text-field%
                       (label "")
                       (parent frame)
-                      (init-value "0")
-                      (enabled #f)))
+                      (init-value "0")))
 
 ;; Row 1: 9 8 7 +
 (define row1 (new horizontal-panel% [parent frame]))
@@ -124,7 +134,7 @@
                  (push-number button))])
 
 (new button% [parent row3]
-     [label "*"]
+     [label "×"]
      [min-width 30]
      [callback (λ (button event)
                  (push-operation button))])
@@ -149,11 +159,11 @@
 (new button% [parent row4]
      [label "="]
      [min-width 30]
-     [callback (λ (button event) ; ASK: do we still need the args if unused?
-                 push-equal)])
+     [callback (λ (b e) ; ASK: do we still need the args if unused?
+                 (push-equal))])
 
 (new button% [parent row4]
-     [label "/"]
+     [label "÷"]
      [min-width 30]
      [callback (λ (button event)
                  (push-operation button))])
